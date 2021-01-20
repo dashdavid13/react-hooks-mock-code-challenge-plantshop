2d1245a327e0279b19b695b40bcce6306eb10f86
dashdavid13 <davidamedina619@gmail.com>
2021-01-20 00:54:16 -0500

    done

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants, onDeletePlant, onUpdatePlant}) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plants.map( plant => (
+      <PlantCard key={plant.id} name={plant.name} image={plant.image} price={plant.price} id={plant.id} onDeletePlant={onDeletePlant} onUpdatePlant={onUpdatePlant}/>
+    ))}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(data => setPlants(data))
+  }, [])
+
+  function onAddPlant(newPlant){
+    setPlants([...plants,newPlant])
+  }
+
+  function handleOnDeletePlant(id){
+      const updatedPlant = plants.filter(plant => plant.id !==id)
+      setPlants(updatedPlant)
+  }
+
+  function handleSearchChange(newSearch){
+    setSearch(newSearch)
+  }
+
+  function handleUpdatePlant(updatedPlant) {
+    const updatedPlantsArray = plants.map((plant) => {
+      if (plant.id === updatedPlant.id) {
+        return updatedPlant;
+      } else {
+        return plant;
+      }
+    });
+    setPlants(updatedPlantsArray);
+  }
+  const displayedPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search search={search}  handleSearchChange={handleSearchChange}/>
+      <PlantList plants={displayedPlants} 
+      onDeletePlant={handleOnDeletePlant}
+      onUpdatePlant={handleUpdatePlant}
+      />
     </main>
   );
 }
